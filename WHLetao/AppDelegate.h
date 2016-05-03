c698b7d02b747ccf6cbd5824a41b2c553d7968fc
Wayne <15210876095@163.com>
2016-05-03 14:33:10 +0800

    letao app
    letao

---
+++ WHLetao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHLetao
+//
+//  Created by Wayne on 16/5/3.
+//  Copyright © 2016年 WayneHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
